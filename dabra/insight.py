80bbc04f5f655161bf6f7d75b5de0fb0284651a8
Marek Ciklamini <marekciklamini@gmail.com>
2023-12-15 12:20:08 +0100

    Update insight.py

--- dabra/insight.py
+++ dabra/insight.py
@@ -7,25 +7,32 @@ import glob
 from pathlib import Path
 
 class emts(object):
-    def __init__(self):
-        self.path2emts = '../../data/0409/*.emt'
-        pass
+    def __init__(self, path2emts = None):
+        if not path2emts: 
+            self.path2emts = '../../data/0409'
+            self.seeker_emts = self.path2emts + '/*.emt'
+        else: 
+            self.path2emts = path2emts
+            self.seeker_emts = self.path2emts + '/**/*.emt'
+        
+        
+        # self.list_emts = glob.glob(self.seeker_emts)
+        self.list_emts = glob.glob(self.seeker_emts, recursive=True)
     def get_emt_list(self, ):
-        self.list_emts = glob.glob(self.path2emts)
         self.list_emts_paths = [Path(x) for x in self.list_emts ]
         # self.list_emts_base = [x.split('/data/0409') for x in self.list_emts]
-        self.list_emts_split = [x.split('/data/0409') for x in self.list_emts]
+        # self.list_emts_split = [x.split('/data/0409') for x in self.list_emts]
         self.list_emts_names = [x.name for x in self.list_emts_paths]
         self.list_emts_stems = [x.stem for x in self.list_emts_paths]        
         return self.list_emts
     def print_emts_stems(self,):
         # [print(f' {x} ''\n') for x in self.list_emts_stems]        
-        [print(f'\item {x} ''\n') for x in self.list_emts_stems]        
+        # [print(f'\item {x} ''\n') for x in self.list_emts_stems]        
+        [print(f'{x} ') for x in self.list_emts_stems]        
+    # def 
     def get_emt_dict(self):
-        
         self.emt_dict = {}
-    
-        
+        pass
     
 def scatter3d_matplot(dataframe_):
     fig = plt.figure()
