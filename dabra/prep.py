3eaf942347d453509a374840ea6d6ce5d9c7ddec
Marek Ciklamini <marekciklamini@gmail.com>
2024-01-05 15:54:40 +0100

    Create prep.py

---
+++ dabra/prep.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+import torch
+def create_dataset(dataset, lookback):
+    """Transform a time series into a prediction dataset
+    
+    Args:
+        dataset: A numpy array of time series, first dimension is the time steps
+        lookback: Size of window for prediction
+    """
+    X, y = [], []
+    for i in range(len(dataset)-lookback):
+        feature = dataset[i:i+lookback]
+        target = dataset[i+1:i+lookback+1]
+        X.append(feature)
+        y.append(target)
+    return torch.tensor(X), torch.tensor(y)
